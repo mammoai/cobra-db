3bc82afefdefc55adc66dd7e41d4d2e0af06e428
huili61 <lihui_lois@163.com>
2023-05-22 17:25:04 +0200

    format: pre-commit

--- tests/test_mongo_dao.py
+++ tests/test_mongo_dao.py
@@ -1,7 +1,16 @@
+from dataclasses import dataclass
+from pathlib import Path
+
 import pytest
 
-from cobra_db.model import FileSource, ImageMetadata, Patient
-from cobra_db.mongo_dao import Connector, ImageMetadataDao, PatientDao, SeriesDao
+from cobra_db.model import Entity, FileSource, ImageMetadata, Patient
+from cobra_db.mongo_dao import (
+    Connector,
+    EntityDao,
+    ImageMetadataDao,
+    PatientDao,
+    SeriesDao,
+)
 
 # ImageMetadataDao, SeriesDao, StudyDao
 
@@ -45,7 +54,9 @@ def test_image_metadata_dao(mongodb):
     im_dao = ImageMetadataDao(connector)
     im = im_dao.get_by_id("62a744bd2f79288ae72033a6")
     assert im.get_acquisition_datetime() is None
-    assert im.get_local_filepath({"drive_1": "/x"}) == "/x/path_to_file/myfile.dcm"
+    assert Path(im.get_local_filepath({"drive_1": "/x"})) == Path(
+        "/x/path_to_file/myfile.dcm"
+    )
     im_dao.insert_one(ImageMetadata(None, None, {}, FileSource("1", "2", "222/3.txt")))
     im_dao.update_series_id("62a744bd2f79288ae72033a6", "62a744bd2f79288ae72033a7")
 
@@ -56,3 +67,24 @@ def test_radiological_series_dao(mongodb):
     connector.db = mongodb
     series_dao = SeriesDao(connector)
     assert series_dao.get_all_ids("62a744bd2f79288ae72033a6") == []
+
+
+def test_entity_dao_insert_one(mongodb):
+    connector = Connector("test.host.com", 27017, "test_db")
+    # the connector db has to be overriden to use the mock database
+    connector.db = mongodb
+
+    # Define a new simple class
+    @dataclass
+    class FunnyEntity(Entity):
+        joke: str
+        joke_response: str = None
+
+    # Create a Dao
+    entity_dao = EntityDao(connector, FunnyEntity)
+
+    funny_instance = FunnyEntity(_id=None, _metadata=None, joke="Whos this?")
+    _id = entity_dao.insert_one(funny_instance)
+    instance_dict = connector.db.FunnyEntity.find_one({"_id": _id})
+    assert instance_dict["joke"] == "Whos this?"
+    assert instance_dict.get("joke_response", 1) == 1
